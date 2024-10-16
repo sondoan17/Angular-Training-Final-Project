1de3a6434af769c7314da2767cd10984d0d9cf1b
sondoan17 <sondoan17@gmail.com>
2024-10-16 17:11:41 +0700

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,23 @@ import { RouterOutlet } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { featherAirplay } from '@ng-icons/feather-icons';
+import { heroUsers } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSidenavModule, NavbarComponent, SidebarComponent],
+  imports: [
+    RouterOutlet,
+    MatSidenavModule,
+    NavbarComponent,
+    SidebarComponent,
+    NgIconComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  viewProviders: [provideIcons({ featherAirplay, heroUsers })],
 })
 export class AppComponent {
   title = 'Angular-Training-Final-Project';
