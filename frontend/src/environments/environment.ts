510cf229c4d312d321daa23d0a7d4a7eff565b93
sondoan17 <sondoan17@gmail.com>
2024-11-04 12:37:40 +0700

    update

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000',
-  googleClientId:
-    '1050818393271-h7sovbi2l89odsg348ik697814lrv3hm.apps.googleusercontent.com',
-  serverUrl: 'http://localhost:3000',
+  production: true,
+  apiUrl: process.env['BACKEND_URL'] || 'http://localhost:3000',
+  googleClientId: process.env['GOOGLE_CLIENT_ID'] || '1050818393271-h7sovbi2l89odsg348ik697814lrv3hm.apps.googleusercontent.com',
+  serverUrl: process.env['SERVER_URL'] || 'http://localhost:3000',
 };
