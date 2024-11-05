b9c35122365cd72d8a613c0f10f087204b8a09ec
sondoan17 <sondoan17@gmail.com>
2024-11-05 12:33:17 +0700

    new commit

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: '/api',
-  googleClientId: '1050818393271-h7sovbi2l89odsg348ik697814lrv3hm.apps.googleusercontent.com',
-  serverUrl: ''
+  apiUrl: 'https://planify-server.vercel.app',
+  googleClientId:
+    '1050818393271-h7sovbi2l89odsg348ik697814lrv3hm.apps.googleusercontent.com',
+  serverUrl: 'https://planify-server.vercel.app',
 };
